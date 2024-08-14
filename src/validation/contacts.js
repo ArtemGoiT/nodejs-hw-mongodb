149a470d7415415cd3e4982a312e1b477705bb45
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-08-14 16:59:14 +0200

    ss

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,7 +3,8 @@ import Joi from 'joi';
 export const createContactsSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string()
-    .pattern(/^[0-9]{10,15}$/)
+    .pattern(/^\+?[0-9]{10,15}$/)
+
     .required(),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
